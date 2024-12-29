11d9474fe7c0dfa78f02afef58c4580ddf9df96b
fspjonny <fabiosilvapedro@yahoo.com.br>
2024-12-29 15:28:53 -0300

    :recycle: refactor: Faço a chave da API ser carregada pelo Streamlit Secrets.

--- app/ia_module.py
+++ app/ia_module.py
@@ -1,17 +1,18 @@
+import streamlit as st
 from groq import Groq
-from decouple import config
 
-# Busca a chave da API da conta.
-key = config('GROQ_API_KEY') 
+# Busca a chave da API do Streamlit Secrets
+key = st.secrets["GROQ_API_KEY"]
 
+# Inicializa o cliente da API com a chave
 client = Groq(api_key=key)
 
 def get_chat_response(messages):
-    """Envio a conversa para a API Groq e ela retorna a resposta do bot."""
+    """Envia a conversa para a API Groq e retorna a resposta do bot."""
     response = client.chat.completions.create(
         messages=messages,
         model="llama3-8b-8192",
     )
 
-    # O Groq já retorna o conteúdo da resposta gerada em formato Markdown.
+    # Retorna o conteúdo da resposta gerada
     return response.choices[0].message.content
